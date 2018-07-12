13be757b7127f1461f0500a7e71b17874144499b
Vladimir <39557447+Vladimir-9341@users.noreply.github.com>
2018-07-12 06:20:10 +0300

    Модуль "Ошибки"

---
+++ AngryBirdsTask.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AngryBirds
+{
+    public static class AngryBirdsTask
+    {
+        public static double FindSightAngle(double v, double distance)
+        {
+            return Math.Asin((9.8 * distance) / (v * v)) / 2;
+        }
+    }
+}
